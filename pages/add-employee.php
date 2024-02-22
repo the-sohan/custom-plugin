b3c3e41e7e778d3461e32c23cba7084d46be091f
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-02-22 23:40:55 +0600

    daily update

--- pages/add-employee.php
+++ pages/add-employee.php
@@ -5,27 +5,43 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
 
 <div class="container">
-  <h2>Vertical (basic) form</h2>
-  <form action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" name="remember"> Remember me</label>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
+   <div class="row">
+      <div class="panel panel-primary">
+         <div class="panel-heading">Add Employee</div>
+
+         <div class="panel-body">
+            <form action="/action_page.php">
+               <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+               </div>
+               <div class="form-group">
+                  <label for="phoneNo">Phone No:</label>
+                  <input type="text" class="form-control" id="phoneNo" placeholder="Enter Phone" name="phoneNo">
+               </div>
+               <div class="form-group">
+                  <label for="gender">Gender:</label>
+                  <select name="gender" class="form-control" id="gender">
+                     <option value="">Select gender</option>
+                     <option value="male">Male</option>
+                     <option value="female">Female</option>
+                  </select>
+               </div>
+               <div class="form-group">
+                  <label for="designation">Designation:</label>
+                  <input type="text" class="form-control" id="designation" placeholder="Enter designation" name="designation">
+               </div>
+               <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+         </div>
+      </div>
+   </div>
 </div>
 
 </body>
--- pages/list-employee.php
+++ pages/list-employee.php
@@ -5,41 +5,94 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="https://cdn.datatables.net/v/dt/dt-2.0.0/datatables.min.css" rel="stylesheet">
 </head>
 <body>
 
-<div class="container">
-  <h2>Bordered Table</h2>
-  <p>The .table-bordered class adds borders to a table:</p>            
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10">
+				<div class="panel panel-primary">
+					<div class="panel-heading">List Employee</div>
+					<div class="panel-body">
+						<table class="table table-bordered" id="tbl-employee">
+							<thead>
+								<tr>
+									<th>#ID</th>
+									<th>#Name</th>
+									<th>#Email</th>
+									<th>#Gender</th>
+									<th>#Designation</th>
+									<th>#Action</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Example User</td>
+									<td>john@example.com</td>
+									<td>Male</td>
+									<td>PHP Developer</td>
+									<td>
+										<a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+										<a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+										<a href="javascript:void(0)" class="btn btn-info">View</a>
+									</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>Example User</td>
+									<td>john@example.com</td>
+									<td>Male</td>
+									<td>PHP Developer</td>
+									<td>
+										<a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+										<a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+										<a href="javascript:void(0)" class="btn btn-info">View</a>
+									</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>Example User</td>
+									<td>john@example.com</td>
+									<td>Male</td>
+									<td>PHP Developer</td>
+									<td>
+										<a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+										<a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+										<a href="javascript:void(0)" class="btn btn-info">View</a>
+									</td>
+								</tr>
+								<tr>
+									<td>4</td>
+									<td>Example User</td>
+									<td>john@example.com</td>
+									<td>Male</td>
+									<td>PHP Developer</td>
+									<td>
+										<a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+										<a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+										<a href="javascript:void(0)" class="btn btn-info">View</a>
+									</td>
+								</tr>
+								
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+
+  <script> 
+    $(function(){
+      new DataTable('#tbl-employee');
+    });
+  </script>
 
 </body>
 </html>
